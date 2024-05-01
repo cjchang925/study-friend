b183454a5cb551d88a1520cb94079d786b6eed44
Chi-Chun Chang <cjchang925@gmail.com>
2024-05-01 12:09:59 +0800

    調整預設色系為亮色系

--- src/app/shared/service/content.service.ts
+++ src/app/shared/service/content.service.ts
@@ -28,7 +28,7 @@ export class ContentService {
   /**
    * 背景模式，light 為淺色，dark 為深色
    */
-  public colorMode: 'light' | 'dark' = 'dark';
+  public colorMode: 'light' | 'dark' = 'light';
 
   /**
    * 設定倒數計時
